0456827609b75f0fd6ec2d3e3d55c793396ab453
Maciej Jarzębkowski <maciej.jarzebkowski@gmail.com>
2020-08-01 02:53:25 +0200

    Move models files

---
+++ Models/ActionTypes.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lore_EF_DB_Structure.Model
+{
+    public class ActionTypes
+    {
+        
+    }
+}
---
+++ Models/Step.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NodaTime;
+using Npgsql.TypeHandlers;
+
+namespace Lore_EF_DB_Structure.Model
+{
+    public class Step
+    {
+        long StepId { get; set; }
+
+        int StepType { get; set; }
+
+        string StepName { get; set; }
+
+        NodaTime.LocalDateTime StepCreateTime { get; set; }
+
+        NodaTime.LocalDateTime StepUpdateTime { get; set; }
+
+        UuidHandler StepUuid { get; set; }
+
+        bool StepIsActive { get; set; }
+
+        bool StepIsDeleted { get; set; }
+    }
+}
